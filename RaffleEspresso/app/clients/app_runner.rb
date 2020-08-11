7348e4ac46605764d13dc85cd29d630ec8806edc
Alexander Nakos <alex.nakos9@gmail.com>
2020-08-10 23:57:49 -0500

    Clean ups

--- RaffleEspresso/app/clients/app_runner.rb
+++ RaffleEspresso/app/clients/app_runner.rb
@@ -5,14 +5,16 @@ require 'csv'
 
 file_name = InputService.get_file_name_input
 p "File_name to s", file_name.to_s
+
 csv_lines = []
 CSV.foreach(file_name) do |row|
   p row
   csv_lines << row
 end
+
 p "Line - count", csv_lines.count
 p "Table readout from #{file_name}:"
 csv_lines.each do |line|
-  p line
+  p line.compact
 end
 
