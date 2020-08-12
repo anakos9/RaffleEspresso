18a6db0671cd498946585a83be082a50f48714a5
Alexander Nakos <alex.nakos9@gmail.com>
2020-08-12 01:35:09 -0500

    Updates.

--- RaffleEspresso/app/clients/app_runner.rb
+++ RaffleEspresso/app/clients/app_runner.rb
@@ -4,75 +4,57 @@ require 'csv'
 require 'net/smtp'
 require 'simple_mail_builder'
 require 'mail'
-# FOR THIS TO WORK THE USER MUST TURN ON 'ALLOW LESS SECURE APPS IN GMAIL SETTINGS'
+
+# FOR THIS TO WORK THE USER MUST TURN ON 'ALLOW LESS SECURE APPS IN GMAIL SETTINGS
 
 file_name = InputService.get_file_name_input
+
+recipient_email = InputService.get_recipient_name
+
 p "File_name to s", file_name.to_s
 
 csv_lines = []
 CSV.foreach(file_name) do |row|
-  p row
   csv_lines << row
 end
 
 p "Line - count", csv_lines.count
 p "Table readout from #{file_name}:"
+
+count = 0
+
 csv_lines.each do |line|
-  p line.compact
+
+  email = line.compact[0]
+  pass = line.compact[1]
+
+  p "Beginning Mail Send Attempt from #{email}"
+
+  Mail.defaults do
+    delivery_method :smtp, { :address    => "smtp.gmail.com",
+                            :port       => 587,
+                            :user_name  => "#{email}",
+                            :password   => "#{pass}",
+                            :authentication => :plain,
+                            :enable_starttls_auto => true
+                          }
+  end
+
+  count = count + 1
+  
+  p "Sending mail now..."
+
+  mail = Mail.new do
+    from     "#{email}"
+    to       "#{recipient_email}"
+    subject  "Raffle Email Test #{count}"
+    body     "Test content ##{count}"
+    #add_file :filename => 'somefile.png', :content => File.read('/somefile.png')
+  end
+
+  p "Mail successfully sent to #{email}!"
+  p "Mail receipt:", mail
+
+  mail.deliver!
+
 end
-
-# FOR THIS TO WORK THE USER MUST TURN ON 'ALLOW LESS SECURE APPS IN GMAIL SETTINGS'
-# p "Beginning Mail Send Attempt from Alex.nakos9@gmail.com"
-
-# Mail.defaults do
-#   delivery_method :smtp, { :address    => "smtp.gmail.com",
-#                           :port       => 587,
-#                           :user_name  => 'toesrpurple@gmail.com',
-#                           :password   => "1234@1234aA",
-#                           :authentication => :plain,
-#                           :enable_starttls_auto => true
-#                         }
-# end
-# p "Sending mail now..."
-# mail = Mail.new do
-#   from     'toesrpurple@gmail.com'
-#   to       'alex@rentgrata.com'
-#   subject  'TEST MAIL FROM RAILS APP'
-#   body     'Hey there!'
-#   #add_file :filename => 'somefile.png', :content => File.read('/somefile.png')
-# end
-
-#p "Mail successfully sent!" if mail.present?
-#p "Mail receipt:", mail
-
-# mail.deliver!
-
-# message = SimpleMailBuilder::Message.new(
-#   to: {'Recipient' => 'Alex.nakos9@gmail.com'},
-#   from: 'alex@rentgrata.com',
-#   reply_to: 'reply-to@domain.com',
-#   subject: 'Test Email',
-#   text: "Header\n\nFirst line\nLast line",
-#   html: '<h1>Header</h1><p>First line</p><p>Last line</p>'
-# ).to_s
-# Net::SMTP.start('localhost', 1025) do |smtp|
-#   smtp.send_message message, 'alex@rentgrata.com', 'Alex.nakos9@gmail.com'
-# end
-
-# message = <<MESSAGE_END
-# From: Private Person <alex@rentgrata.com>
-# To: A Test User <Alex.nakos9@gmail.com>
-# MIME-Version: 1.0
-# Content-type: text/html
-# Subject: SMTP e-mail test
-
-# This is an e-mail message to be sent in HTML format
-
-# <b>This is HTML message.</b>
-# <h1>This is headline.</h1>
-# MESSAGE_END
-
-# Net::SMTP.start('localhost') do |smtp|
-#   smtp.send_message message, 'alex@rentgrata.com', 
-#                              'Alex.nakos9@gmail.com'
-# end
--- RaffleEspresso/app/clients/input_service.rb
+++ RaffleEspresso/app/clients/input_service.rb
@@ -1,10 +1,15 @@
 class InputService
 
   def self.get_file_name_input
-    puts "Please enter your CSV file name:"
+    puts "Please enter the name of the CSV file you would like to use:"
     name = gets.chomp
-    p "Name->", name
     return name
+  end
+
+  def self.get_recipient_name
+    puts "Please enter the email of where you would like to submit your entries to:"
+    email = gets.chomp
+    return email
   end
 
 end
